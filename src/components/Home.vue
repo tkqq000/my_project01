f4a1628b35d21cafa08f8756b05a2bad84d6fe72
taoke <827912366@qq.com>
2020-04-06 11:10:44 +0800

    master

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <el-button type="info" @click="logout">退出</el-button>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -8,11 +18,11 @@
 export default {
   methods: {
     logout() {
-      window.sessionStorage.clear()
-      this.$router.push('/login')
+      window.sessionStorage.clear();
+      this.$router.push("/login");
     }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
